9a350bd1094f830e6d912ee64cdc55283735c1de
SaadeHub <130802772+SaadeHub@users.noreply.github.com>
2023-04-17 23:44:23 +0200

    Add files via upload

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,76 +1,320 @@
-if game.PlaceId == 13106612654 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/SaadeHub/hub/main/123.lua"))()
---FiveR
- --[[
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---Leeuwarden
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-  elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---]]
-elseif game.PlaceId == 7009799230 then
-loadstring(game:HttpGet("https://pastebin.com/raw/V1AeGyER"))()
---Pressure Wash Simulator
-  
-  
-  
-  
-  
-  
-end
-
-
---[[
-elseif game.PlaceId == then
-loadstring(game:HttpGet(""))()
---
-]]
-
+    local Rayfield = loadstring(game:HttpGet('https://pastebin.com/raw/yp0BjnBy'))()
+        local Window = Rayfield:CreateWindow({
+           Name = "Saade Hub - Pressure Wash Simulator",
+           LoadingTitle = "Saade Hub",
+           LoadingSubtitle = "By MK#3137",
+           ConfigurationSaving = {
+              Enabled = false,
+              FolderName = true, -- Create a custom folder for your hub/game
+              FileName = "Saade Hub"
+           },
+           Discord = {
+              Enabled = true,
+              Invite = "saadehub", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+              RememberJoins = false -- Set this to false to make them join the discord every time they load it up
+           },
+           KeySystem = true, -- Set this to true to use our key system
+           KeySettings = {
+              Title = "Saade Hub",
+              Subtitle = "discord.gg/saadehub",
+              Note = "Join our Discord for the key to get acces!",
+              FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+              SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+              GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+              Key = {"https://pastebin.com/raw/1tU9NDxp"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+           }
+        })
+        
+        
+        local Tab = Window:CreateTab("Profile", 6035053288)
+        local Slider = Tab:CreateSlider({
+            Name = "WalkSpeed",
+            Range = {16, 100},
+            Increment = 2,
+            Suffix = "WalkSpeed",
+            CurrentValue = 16,
+            Flag = "Slider1",
+            Callback = function(s)
+                wait()
+                game.Players.LocalPlayer.character.Humanoid.WalkSpeed = s
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Freecam",
+            Callback = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/n3HAqKgs"))()
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "LeftControl Delete",
+            Callback = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/vMQpXxWB"))()
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "LeftShift Teleport",
+            Callback = function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/bf3GDt6F"))()
+            end,
+        })
+        local Section = Tab:CreateSection("Remove Saade Hub")
+        local Button = Tab:CreateButton({
+            Name = "Destroy",
+            Callback = function()
+            Rayfield:Destroy()
+            end,
+        })
+        local Tab = Window:CreateTab("Main", 6034767613)
+        local Button = Tab:CreateButton({
+            Name = "Unlimited Cash",
+            Callback = function()
+                local money = 99e400
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Spam to complete zone",
+            Callback = function()
+                local money = 9000
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(0, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+                game:GetService("ReplicatedStorage").Remotes.SurfaceCompleted:FireServer(1, money)
+            end,
+        })
+        local Toggle = Tab:CreateToggle({
+            Name = "Unlimited Water",
+            CurrentValue = false,
+            Flag = "Toggle1",
+            Callback = function(Value)
+                if Value == true then
+                    game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(true)
+                elseif Value == false then
+                    game:GetService("ReplicatedStorage").Remotes.RefillRemote:FireServer(false)
+                end
+            end,
+        })
+    
+    local Tab = Window:CreateTab("Spawners", 6034754445)
+    local Section = Tab:CreateSection("May have to buy them first")
+    local Button = Tab:CreateButton({
+        Name = "Wooden Boat",
+        Callback = function()
+            local ohString1 = "Wooden Boat"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Button = Tab:CreateButton({
+        Name = "Medium Boat",
+        Callback = function()
+            local ohString1 = "Medium Boat"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Button = Tab:CreateButton({
+        Name = "Steam Boat",
+        Callback = function()
+            local ohString1 = "Steam Boat"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Button = Tab:CreateButton({
+        Name = "Yacht",
+        Callback = function()
+            local ohString1 = "Yacht"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Button = Tab:CreateButton({
+        Name = "Pirate Ship",
+        Callback = function()
+            local ohString1 = "Pirate Ship"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Button = Tab:CreateButton({
+        Name = "Jetski",
+        Callback = function()
+            local ohString1 = "Jetski"
+            game:GetService("ReplicatedStorage").Remotes.SpawnBoat:FireServer(ohString1)
+        end,
+    })
+    local Tab = Window:CreateTab("Teleports", 6026568211)
+    local Section = Tab:CreateSection("World 1")
+        local Button = Tab:CreateButton({
+            Name = "Spawn",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(217, 36, 156)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Parking Lot",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(65, 34, 157)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Garden",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-100, 35, 163)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Mansion",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-257, 34, 162)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "School",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-395, 34, 154)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Airport",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-554, 33, 167)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Laboratory",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-704, 34, 166)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Artic",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-881, 34, 154)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Castle",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1035, 36, 168)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Mars",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1231, 34, 162)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Cleandromeda",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1406, 35, 152)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Bank",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1639, 34, 179)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Jewelry Store",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1788, 35, 172)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Titanic",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1979, 40, 161)
+            end,
+        })
+    
+        local Section = Tab:CreateSection("World 2")
+        local Button = Tab:CreateButton({
+            Name = "Spawn",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(900, 42, 3159)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Planet Arcade",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(769, 38, 3138)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "City",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(532, 38, 3138)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Prison",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(336, 37, 3121)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Sewers",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(137, 37, 3119)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Blockland",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-95, 37, 3122)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Nether",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-305, 37, 3096)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Carnival",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-522, 36, 3068)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Giant Picnic",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-782, 35, 3088)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Nostalgia",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1084, 37, 3097)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Skatepark",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1318, 35, 3097)
+            end,
+        })
+        local Section = Tab:CreateSection("Islands")
+        local Button = Tab:CreateButton({
+            Name = "Pirate Cove",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(86, 30, -851)
+            end,
+        })
+        local Button = Tab:CreateButton({
+            Name = "Haunted Island",
+            Callback = function()
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1910, 35, -1719)
+            end,
+        })
+    
+    
+    
+    
+    
