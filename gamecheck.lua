dda831c5b6ba4e9474821efac21de99aeb5e35c5
SaadeHub <130802772+SaadeHub@users.noreply.github.com>
2023-04-17 23:39:24 +0200

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,1 +1,76 @@
+if game.PlaceId == then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaadeHub/hub/main/123.lua"))()
+--FiveR
+ --[[
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--Leeuwarden
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+  elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--]]
+elseif game.PlaceId == 7009799230 then
+loadstring(game:HttpGet("https://pastebin.com/raw/V1AeGyER"))()
+--Pressure Wash Simulator
+  
+  
+  
+  
+  
+  
+end
+
+
+--[[
+elseif game.PlaceId == then
+loadstring(game:HttpGet(""))()
+--
+]]
 
