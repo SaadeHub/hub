5e062a9e678a37ba619b5fba555130829e25fbbb
SaadeHub <130802772+SaadeHub@users.noreply.github.com>
2023-04-17 23:47:47 +0200

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,4 +1,4 @@
 if game.PlaceId == 7009799230 then
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaadeHub/hub/main/PWS.lua"))()
 --Pressure Wash Simulator
 end
