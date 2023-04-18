b7a48be9b993172c1a98b6c1c4664dd1da017c19
SaadeHub <130802772+SaadeHub@users.noreply.github.com>
2023-04-18 06:30:55 +0200

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,4 +1,7 @@
 if game.PlaceId == 7009799230 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/SaadeHub/hub/main/PWS.lua"))()
 --Pressure Wash Simulator
+elseif game.PlayerId == 5938036553 then
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaadeHub/hub/main/FL.lua"))() 
+--Frontlines
 end
